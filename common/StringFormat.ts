6488e2fcddb88c88a0a22807a6fe5259c045a882
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-12-04 08:36:25 +0000

    own implemented version of java equivalent String.Format

--- common/StringFormat.ts
+++ common/StringFormat.ts
@@ -3,7 +3,6 @@ interface String
     format(...replacements: string[]): string;
 }
 
-// First, checks if it isn't implemented yet.
 if (!String.prototype.format) {
     String.prototype.format = function ()
     {

e3b72faeb07a9864a54aa74d72daa5823c8c10f8
Michael.Butterfield <Michael.Butterfield@ROQ-136>
2018-12-04 08:37:02 +0000

    changed getapiboardid to a bool, allowing for true/false

--- steps/TrelloApiSteps.ts
+++ steps/TrelloApiSteps.ts
@@ -1,39 +1,52 @@
 import { Given, Then, Before } from "cucumber";
-import { TrelloApiHelper } from "../api/TrelloApiHelper";
+import { trello } from "../api/specificapi/trello";
 
 const frisby = require('frisby');
 
 Before(async () =>
 {
-    TrelloApiHelper.ReadApiKeyToken();
+    trello.ReadApiKeyToken();
 });
 
 Given('I try and get the board Id', function ()
+{
+    if (trello.GetApiBoardId("apitestboard"))
+    {
+        return true;
+    }
+    else
+    {
+        try
         {
-            let response = TrelloApiHelper.GetBoardId("dgfasr");
-            console.log(response);
+            throw new Error("Board ID not retrieved properly");
         }
-     );
+        catch(e)
+        {
+            console.log(e);
+        }
+    }
+}
+);
 
 Given('I query Trello to create a new board', function ()
-        {
-            //Assumes JSON and sends the header Content-Type: application/json by default.
-            console.log('https://api.trello.com/1/boards/?name=Javascript Test Board' + '&key=' + TrelloApiHelper.GetApiKey() + '&token=' + TrelloApiHelper.GetApiToken());
-            frisby.post('https://api.trello.com/1/boards/?name=Javascript Test Board' + '&key=' + TrelloApiHelper.GetApiKey() + '&token=' + TrelloApiHelper.GetApiToken())
-            .expect('status', 200);
-        }
-     );
+{
+    //Assumes JSON and sends the header Content-Type: application/json by default.
+    console.log('https://api.trello.com/1/boards/?name=Javascript Test Board' + '&key=' + trello.GetApiKey() + '&token=' + trello.GetApiToken());
+    frisby.post('https://api.trello.com/1/boards/?name=Javascript Test Board' + '&key=' + trello.GetApiKey() + '&token=' + trello.GetApiToken())
+        .expect('status', 200);
+}
+);
 
 Given('I query Trello to delete the board', function ()
-        {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-        }
-     );
+{
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+}
+);
 
 Then('the status response will be {string}', function (input: string)
-        {
-            // Write code here that turns the phrase above into concrete actions
-            return 'pending';
-        }
-    );
+{
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+}
+);
